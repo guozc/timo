03fb1722d07ce5e2e888ca3e202789459935c98d
Guozc <guozhaocheng@whaledream.com>
2017-06-18 14:43:11 +0800

    1、去掉prefix功能 2、增加递归实现 3、现在可为多个key

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 -------------
 * python版本3.5 ，依赖包 tinify
 * timo.bat 设置 timo.py 的目录 以及 Python35的位置,.bat 放在windows目录下
-* timo.py 设置tinypng 的 key
+* timo.py 设置tinypng 的 key,key可以为多个
 * timo.reg 修改相应批处理的名字,这里用的是timo.bat
--- timo.py
+++ timo.py
@@ -1,30 +1,40 @@
 import os,sys
+from urllib.request import Request,urlopen
 import tinify
 import threading
 import time
 
-
-key = "your key " #enter your key
-tinify.key = key
+#key list 现在是多个，免费的账户有每个使用500次的限制
+keys = ["key1","key2","key3"]
 suffix = ["png",'jpg']
 
 global timo_has_done
 timo_has_done = 0
 
-def get_image_filenames(input_dir):
-    image_name_list = []
+def get_image_filenames(input_dir):    
     for fn in os.listdir(input_dir):
         if len(fn.split("."))>1:
             if fn.split(".")[-1] in suffix:
                 image_name_list.append(fn)
     return image_name_list
 
-def do_timo(filename,total,input_dir,output_dir,index,isperfix,perfix):
-    out_name = filename
-    if isperfix:
-        out_name = perfix+str(index)+"."+filename.split(".")[-1]
-    source = tinify.from_file(input_dir+"/"+filename)
-    source.to_file(output_dir+"/"+out_name)
+image_name_list = []
+def get_all_images(input_dir):
+    for fn in os.listdir(input_dir):
+        filepath = os.path.join(input_dir, fn)
+        if os.path.isdir(filepath):           
+            get_all_images(filepath)            
+        if len(filepath.split("."))>1:
+            if filepath.split(".")[-1] in suffix:
+                image_name_list.append(filepath)
+    return image_name_list
+
+
+
+def do_timo(filename,total,now_dir_name,new_dir_name,index):
+    out_name = filename.replace(now_dir_name,new_dir_name,1)
+    source = tinify.from_file(filename)
+    source.to_file(out_name)
     global timo_has_done
     timo_has_done += 1
     print("{0} has done ,total {1},completed {2}".format(out_name,total,timo_has_done))
@@ -32,26 +42,54 @@ def do_timo(filename,total,input_dir,output_dir,index,isperfix,perfix):
 
 def main():
     if(len(sys.argv)>1):
-        mode_str = input("convert perfix (n/y)")
-        isperfix = False
-        perfix = ""
 
-        if mode_str is "yes" or mode_str is "y" or mode_str is "Y":
-            isperfix = True
-            perfix = input("Please enter image perfix:")
-        #批量修改需完善，
-
-        print("convert started ...")
+        print("validate keys ...")
         input_dir = sys.argv[1]
-        output_dir = "./new_image_{0}".format(int(time.time()))
+        now_dir_name = input_dir.split("\\")[-1]
+        new_dir_name = "new_image_{0}".format(int(time.time()))
+        output_dir = "./"+new_dir_name
+
+        #建立新的目录及子目录
+        dir_list = dirlist(input_dir,[],now_dir_name,new_dir_name)
         os.mkdir(output_dir)
-        image_names = get_image_filenames(input_dir)
+        for dir_path in dir_list[::-1]:
+            os.mkdir(dir_path)
+
+        image_names = get_all_images(input_dir)
         total = len(image_names)
-        threads = []
-        for index,image_name in enumerate(image_names):
-           threads.append(threading.Thread(target=do_timo,args=(image_name,total,input_dir,output_dir,index,isperfix,perfix)))
-        for t in threads:
-            t.start()
+
+
+        #判断使用哪个Key,一个Key只能使用500次
+        key_effective = False
+        for key in keys:
+            tinify.key = key
+            tinify.validate()
+            remain = 500-tinify.compression_count
+            if remain>total:
+                key_effective = True
+                break
+        
+        if key_effective:
+            print("now_key:"+tinify.key)
+            print("convert start ...")
+            threads = []
+            for index,image_name in enumerate(image_names):
+               threads.append(threading.Thread(target=do_timo,args=(image_name,total,now_dir_name,new_dir_name,index)))
+            for t in threads:
+                t.start()
+        else:
+            print("please check your key list,compression count may be full !!!")
+       
+def dirlist(path, allfile,now_dir_name,new_dir_name):  
+    filelist =  os.listdir(path)  
+  
+    for filename in filelist:  
+        filepath = os.path.join(path, filename)  
+        if os.path.isdir(filepath):           
+            dirlist(filepath, allfile,now_dir_name,new_dir_name)
+            allfile.append(filepath.replace(now_dir_name,new_dir_name,1))  
+    return allfile
+
 
 if __name__ == '__main__':
     main()
