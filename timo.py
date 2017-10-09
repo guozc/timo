14695438c3c91089debbaf282e9bde36bf058f74
Guozc <guozhaocheng@whaledream.com>
2017-10-09 14:50:04 +0800

    1、修正了scale图片的一个错误 2、增加输入定宽scale图片的方法 3、增加audio转base64的功能

--- timo.py
+++ timo.py
@@ -10,6 +10,7 @@ import json
 
 keys = ["yourkey1","yourkey2","yourkey3"]
 suffix = ["png","jpg","JPG","JEPG","jepg","PNG"]
+suffixMp3 = ["mp3","MP3"]
 
 global timo_has_done
 timo_has_done = 0
@@ -35,6 +36,17 @@ def get_all_images(input_dir):
                 image_name_list.append(filepath)
     return image_name_list
 
+audio_name_list = []
+def get_all_audios(input_dir):
+    for fn in os.listdir(input_dir):
+        filepath = os.path.join(input_dir, fn)
+        if os.path.isdir(filepath):           
+            get_all_audios(filepath)            
+        if len(filepath.split("."))>1:
+            if filepath.split(".")[-1] in suffixMp3:
+                audio_name_list.append(filepath)
+    return audio_name_list
+
 def removeDir(dirPath):
     if not os.path.isdir(dirPath):
         return
@@ -57,25 +69,43 @@ def dirlist(path, allfile,now_dir_name,new_dir_name):
             allfile.append(filepath.replace(now_dir_name,new_dir_name,1))  
     return allfile
 
-def do_timo(filename,total,now_dir_name,new_dir_name,index,scale_percentage):
+#mode 1 根据百分比;2 根据宽
+def do_timo(filename,total,now_dir_name,new_dir_name,index,scale_num,mode):
     out_name = filename.replace(now_dir_name,new_dir_name,1)
-
-    if scale_percentage < 1:
-        image_width = Image.open(filename).size[0]
-        image_height = Image.open(filename).size[1]
-        source = tinify.from_file(filename)
-        resized = source.resize(
-            method="fit",
-            width = image_width*scale_percentage,
-            height = image_height*scale_percentage
-        )
-        resized.to_file(out_name)
+  
+    if mode is 2:
+        scaleWidth = scale_num
+        if scaleWidth is not None and scaleWidth is not "":
+            source = tinify.from_file(filename)
+            resized = source.resize(
+                method="scale",
+                width = scaleWidth
+            )
+            resized.to_file(out_name)
+        else:
+            source = tinify.from_file(filename)
+            source.to_file(out_name)            
     else:
-        source = tinify.from_file(filename)
-        source.to_file(out_name)
+        scale_percentage = scale_num
+        if scale_percentage < 1:
+            image_width = Image.open(filename).size[0]
+            image_height = Image.open(filename).size[1]
+            source = tinify.from_file(filename)
+            resized = source.resize(
+                method="fit",
+                width = int(image_width*scale_percentage),
+                height = int(image_height*scale_percentage)
+            )
+            resized.to_file(out_name)
+        else:
+            source = tinify.from_file(filename)
+            source.to_file(out_name)
+
     global timo_has_done
     timo_has_done += 1
     print("{0} has done ,total {1},completed {2}".format(out_name,total,timo_has_done))
+
+
 
 preimage = "data:image/png;base64,"
 def do_base64(filename,total,now_dir_name,new_dir_name,index):    
@@ -89,6 +119,17 @@ def do_base64(filename,total,now_dir_name,new_dir_name,index):
     base64_has_done += 1
     print("{0} has done ,total {1},completed {2}".format(filename.replace(now_dir_name,new_dir_name)+".txt",total,base64_has_done))
 
+preaudio = "data:audio/mp3;base64,"
+def do_base64_audio(filename,total,now_dir_name,new_dir_name,index):    
+    from_png = open(filename,'rb')
+    base_temp = base64.b64encode(from_png.read())  
+    to_txt = open(filename.replace(now_dir_name,new_dir_name)+".txt",'w')
+    to_txt.write(preaudio+str(base_temp)[2:-1])
+    from_png.close()
+    to_txt.close()
+    global base64_has_done
+    base64_has_done += 1
+    print("{0} has done ,total {1},completed {2}".format(filename.replace(now_dir_name,new_dir_name)+".txt",total,base64_has_done))
 
 def do_tinify_list():
     print("validate keys ...")
@@ -118,25 +159,56 @@ def do_tinify_list():
 
     if key_effective:
         print("now_key:"+tinify.key)
-        #get scale 
-        scale_percentage = input("enter scale_percentage,0<scale_percentage<=100,default = 100 :")
-        try:
-            scale_percentage = float(scale_percentage);
-            if scale_percentage>=100.0 or scale_percentage<0:
-                scale_percentage = 100.0
-        except:
-            scale_percentage = 100.0
-        print("convert scale_percentage is "+str(scale_percentage)+"%")
-        scale_percentage = scale_percentage/100
+        #get scale
 
-        print("convert start ...")
-        threads = []
-        for index,image_name in enumerate(image_names):
-           threads.append(threading.Thread(target=do_timo,args=(image_name,total,now_dir_name,new_dir_name,index,scale_percentage)))
-        for t in threads:
-            t.start()
+        mode = input("\nselect scale mode :\n[1]scale by percentage\n[2]scale by fixed width\nenter your choice (default=1):")
+        if mode is not "2" :
+            mode = 1
+        else:
+            mode = 2
+        print("Scale mode is {0}".format(mode))
+
+        if mode is 1:
+            scale_percentage = input("enter scale_percentage,0<scale_percentage<=100,default = 100 :")
+            try:
+                scale_percentage = float(scale_percentage);
+                if scale_percentage>=100.0 or scale_percentage<0:
+                    scale_percentage = 100.0
+            except:
+                scale_percentage = 100.0
+            print("convert scale_percentage is "+str(scale_percentage)+"%")
+            scale_percentage = scale_percentage/100
+
+            print("convert start ...")
+            threads = []
+            for index,image_name in enumerate(image_names):
+               #threads.append(threading.Thread(target=do_timo,args=(image_name,total,now_dir_name,new_dir_name,index,scale_percentage,2)))
+               threads.append(threading.Thread(target=do_timo,args=(image_name,total,now_dir_name,new_dir_name,index,scale_percentage,1)))
+            for t in threads:
+                t.start()
+        elif mode is 2:
+            fixedWidth = input("enter fixed width(integer) ,default or wrong input cause no scale:")
+            try:
+                fixedWidth = int(fixedWidth)
+                print("fixed width is {0}".format(fixedWidth))
+            except:
+                fixedWidth = None
+                print("will be no scale")
+
+            print("convert start ...")
+            threads = []
+            for index,image_name in enumerate(image_names):
+               #threads.append(threading.Thread(target=do_timo,args=(image_name,total,now_dir_name,new_dir_name,index,scale_percentage,2)))
+               threads.append(threading.Thread(target=do_timo,args=(image_name,total,now_dir_name,new_dir_name,index,fixedWidth,2)))
+            for t in threads:
+                t.start()
     else:
         print("please check your key list,compression count may be full !!!")
+
+
+
+
+
 
 def do_base64_list():
     input_dir = sys.argv[1]
@@ -159,6 +231,30 @@ def do_base64_list():
     threads = []
     for index,image_name in enumerate(image_names):
        threads.append(threading.Thread(target=do_base64,args=(image_name,total,now_dir_name,new_dir_name,index)))
+    for t in threads:
+        t.start()
+
+def do_base64_list_audio():
+    input_dir = sys.argv[1]
+    now_dir_name = input_dir.split("\\")[-1]
+    new_dir_name = "base64_"+now_dir_name
+    output_dir = "./"+new_dir_name
+
+    #建立新的目录及子目录
+    dir_list = dirlist(input_dir,[],now_dir_name,new_dir_name)
+    if os.path.exists(output_dir):
+        removeDir(output_dir)
+    os.mkdir(output_dir)
+    for dir_path in dir_list[::-1]:
+        os.mkdir(dir_path)
+
+    audio_names = get_all_audios(input_dir)
+    total = len(audio_names)
+
+    print("convert base64 start ...")
+    threads = []
+    for index,audio_name in enumerate(audio_names):
+       threads.append(threading.Thread(target=do_base64_audio,args=(audio_name,total,now_dir_name,new_dir_name,index)))
     for t in threads:
         t.start()
 
@@ -190,7 +286,7 @@ def do_get_all_imagePath():
 
 def main():
     if(len(sys.argv)>1):
-        feature = input("choose function: [1]tinify; [2]toBase64 ; [3]clip image rename ;[4] get all imagePath:")
+        feature = input("choose function: \n[1]tinify\n[2]ImageToBase64;\n[3]clip image rename\n[4]get all imagePath\n[5]AudioToBase64;\nenter your choice and press enter:")
 
         #tinify
         if feature == "1":
@@ -208,5 +304,7 @@ def main():
             do_clip_rename(start_num)
         elif feature == "4":
             do_get_all_imagePath()
+        elif feature == "5":
+            do_base64_list_audio()
 if __name__ == '__main__':
     main()
